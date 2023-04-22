8858f3a3db2853b5fdf545249a9a20a79e574335
StasySmile <anastasiia.polieshko@gmail.com>
2023-04-23 01:54:16 +0300

    Added code by Denis Savran

---
+++ src/login.js
@@ -0,0 +1,19 @@
+// Open the second pop-up
+const openPopUp2 = document.getElementById("open_pop_up_2");
+const popUp2 = document.getElementById("pop_up_2");
+const popUpBody2 = document.getElementById("pop_up_body_2");
+const popUpClose2 = document.getElementById("pop_up_close_2");
+
+openPopUp2.addEventListener("click", () => {
+  popUp2.classList.add("active");
+});
+
+// Close the second pop-up
+popUpClose2.addEventListener("click", () => {
+  popUp2.classList.remove("active");
+});
+popUp2.addEventListener("click", (e) => {
+  if (e.target === popUp2) {
+    popUp2.classList.remove("active");
+  }
+});
