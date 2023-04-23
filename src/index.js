e4017d8f5dd4a0b193cd7d0e7a90f8ca3e6fd96e
StasySmile <anastasiia.polieshko@gmail.com>
2023-04-23 03:18:22 +0300

    Added form validation for Add unit page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
+const adressEl = document.querySelector("#inputAdress");
+const areaEl = document.querySelector("#inputArea");
+const roomsEl = document.querySelector("#inputRooms");
+const priceEl = document.querySelector("#inputPrice");
+const notesEl = document.querySelector("#inputNotes");
+
+const isRequired = (value) => (value === "" ? false : true);
+const isBetween = (length, min, max) =>
+  length < min || length > max ? false : true;
+
+const showError = (input, message) => {
+  const formField = input.parentElement;
+  formField.classList.remove("success");
+  formField.classList.add("error");
+  const error = formField.querySelector("small");
+  error.textContent = message;
+};
+
+const showSuccess = (input) => {
+  const formField = input.parentElement;
+  formField.classList.remove("error");
+  formField.classList.add("success");
+  const error = formField.querySelector("small");
+  error.textContent = "";
+};
+
+const checkAdress = () => {
+  let valid = false;
+  const min = 5,
+    max = 100;
+  const adress = adressEl.value.trim();
+  if (!isRequired(adress)) {
+    showError(adressEl, "Поле Адреса не може бути пустим.");
+  } else if (!isBetween(adress.length, min, max)) {
+    showError(adressEl, `Поле Адреса має містити від ${min} до ${max} знаків.`);
+  } else {
+    showSuccess(adressEl);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkArea = () => {
+  let valid = false;
+  const area = areaEl.value.trim();
+  if (!isRequired(area)) {
+    showError(areaEl, "Поле Площа не може бути пустим.");
+  } else {
+    showSuccess(areaEl);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkRooms = () => {
+  let valid = false;
+  const rooms = roomsEl.value.trim();
+  if (!isRequired(rooms)) {
+    showError(roomsEl, "Поле Кількість кімнат не може бути пустим.");
+  } else {
+    showSuccess(roomsEl);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkPrice = () => {
+  let valid = false;
+  const price = priceEl.value.trim();
+  if (!isRequired(price)) {
+    showError(priceEl, "Поле Ціна не може бути пустим.");
+  } else {
+    showSuccess(priceEl);
+    valid = true;
+  }
+  return valid;
+};
+
 function sendJSON(data) {
   let xhr = new XMLHttpRequest();
   let url = "";
@@ -8,9 +86,7 @@ function sendJSON(data) {
   xhr.send(data);
 }
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-
+function handleFormSubmit() {
   const data = new FormData(event.target);
 
   const formJSON = Object.fromEntries(data.entries());
@@ -21,4 +97,17 @@ function handleFormSubmit(event) {
 }
 
 const form = document.querySelector(".new-object-form");
-form.addEventListener("submit", handleFormSubmit);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let isAdressValid = checkAdress(),
+    isAreaValid = checkArea(),
+    isRoomsValid = checkRooms(),
+    isPriceValid = checkPrice();
+  let isFormValid =
+    isAdressValid && isAreaValid && isRoomsValid && isPriceValid;
+  if (isFormValid) {
+    handleFormSubmit();
+  }
+});
+
+
